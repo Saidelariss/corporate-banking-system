fd628694f0e1964942c45d7debc2828ac195e37b
Said EL ARISS EL IDRISSI <saidariss3@gmail.com>
2026-07-05 14:19:52 +0100

    init project

---
+++ src/main/java/com/corporatebanking/CorporateBankingApplication.java
@@ -0,0 +1,13 @@
+package com.corporatebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corporatebanking/CorporateBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corporatebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
